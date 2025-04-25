7d6402ad8b4f7609b54d2da250992ea8cf82dad9
edpau <edpau@me.com>
2025-04-25 15:52:01 +0100

    test: add initial test to check deck size on construction

--- src/test/java/org/example/CardGameTest.java
+++ src/test/java/org/example/CardGameTest.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CardGameTest {
+    private CardGame cardGame;
+
+    @BeforeEach
+    void setUp(){
+        cardGame = new CardGame("Snap");
+    }
+
+    // methodName_StateUnderTest_ExpectedBehaviour
+    @Test
+    void getDeck_WhenCalledAfterConstruction_Return52cards(){
+        assertEquals(52, cardGame.getDeck().size());
+    }
+
 }
