d480ddf3ff2318e305e3bc4156f651b17fd3c774
edpau <edpau@me.com>
2025-04-30 21:41:31 +0100

    feat: add deck shuffling and sorting functions, refactor deck generation

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class CardGame {
@@ -10,27 +12,32 @@ public class CardGame {
     public record SymbolValue(String symbol, int value) {
     }
 
-    ;
+    public static final SymbolValue[] SYMBOLS_AND_VALUES = {
+            new SymbolValue("2", 2), new SymbolValue("3", 3), new SymbolValue("4", 4),
+            new SymbolValue("5", 5), new SymbolValue("6", 6), new SymbolValue("7", 7),
+            new SymbolValue("8", 8), new SymbolValue("9", 9), new SymbolValue("10", 10),
+            new SymbolValue("J", 11), new SymbolValue("Q", 12), new SymbolValue("K", 13),
+            new SymbolValue("A", 14)};
+
+    private static final Comparator<Card> CARD_VALUE_SUIT_COMPARATOR = Comparator.comparingInt(Card::getValue).thenComparing(Comparator.comparing(Card::getSuit).reversed());
+    private static final Comparator<Card> CARD_SUIT_VALUE_COMPARATOR = Comparator.comparing(Card::getSuit).thenComparingInt(Card::getValue);
+
+
+    //
+
+    public static List<Card> generateDeck() {
+        List<Card> deck = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (SymbolValue symbolANdValue : SYMBOLS_AND_VALUES) {
+                deck.add(new Card(suit, symbolANdValue.symbol(), symbolANdValue.value()));
+            }
+        }
+        return deck;
+    }
 
     public CardGame(String name) {
         this.name = name;
-
-        String[] suits = {"♠", "♥", "♣", "♦"};
-        SymbolValue[] symbolsAndValues = {
-                new SymbolValue("2", 2), new SymbolValue("3", 3), new SymbolValue("4", 4),
-                new SymbolValue("5", 5), new SymbolValue("6", 6), new SymbolValue("7", 7),
-                new SymbolValue("8", 8), new SymbolValue("9", 9), new SymbolValue("10", 10),
-                new SymbolValue("J", 11), new SymbolValue("Q", 12), new SymbolValue("K", 13),
-                new SymbolValue("A", 14)
-
-        };
-
-        for (String suit : suits) {
-            for (SymbolValue symbolANdValue : symbolsAndValues) {
-                deckOfCards.add(new Card(suit, symbolANdValue.symbol(), symbolANdValue.value()));
-            }
-        }
-
+        this.deckOfCards = generateDeck();
     }
 
     public List<Card> getDeck() {
@@ -46,6 +53,26 @@ public class CardGame {
         if (deckOfCards.isEmpty()) {
             return null;
         }
-        return deckOfCards.remove(deckOfCards.size()-1);
+        return deckOfCards.remove(deckOfCards.size() - 1);
+    }
+
+
+    // TODO handle when the deck is empty, with exception
+    public void sortDeckInNumberOrder() {
+        if (deckOfCards.isEmpty()) {
+            return;
+        }
+        deckOfCards.sort(CARD_VALUE_SUIT_COMPARATOR);
+    }
+
+    public void sortDeckIntoSuits() {
+        if (deckOfCards.isEmpty()) {
+            return;
+        }
+        deckOfCards.sort(CARD_SUIT_VALUE_COMPARATOR);
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(deckOfCards);
     }
 }
