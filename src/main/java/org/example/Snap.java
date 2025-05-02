de85f371ca0b6f28a190636c32854eedad3fdac1
edpau <edpau@me.com>
2025-05-02 16:34:46 +0100

    refactor: reorder fields and methods in Snap class for clarity

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 
 public class Snap extends CardGame {
     private static final Scanner scanner = new Scanner(System.in);
+
     private final Player playerOne;
     private final Player playerTwo;
-    private int currentPlayerIndex = 0;
     private final List<Player> playerList;
 
+    private int currentPlayerIndex = 0;
     private Card previousCard;
     private Card currentCard;
-
-
 
     public Snap() {
         super("Snap");
@@ -26,7 +25,6 @@ public class Snap extends CardGame {
 
     }
 
-
     public static void promptEnterKey(Player currentPlayer) {
         while (true) {
             System.out.println(currentPlayer.getName() + " please press \"ENTER\" to continue...");
@@ -37,11 +35,79 @@ public class Snap extends CardGame {
         }
     }
 
-    public Card getPreviousCard() {
+    public void playGameLoop() {
+        while (true) {
+            promptEnterKey(currentPlayer());
+            this.currentCard = dealCard();
+            System.out.println("New Card is " + this.currentCard);
+            if (isSymbolMatch(this.getPreviousCard(), this.getCurrentCard())) {
+                handleSnapInput();
+                break;
+            } else if (this.getDeck().isEmpty()) {
+                System.out.println("No card left, draw");
+                break;
+            } else {
+                this.previousCard = this.currentCard;
+                System.out.println(currentPlayer().getName() + ", didn't win");
+                System.out.println("Card left in Deck " + this.getDeck().size());
+                switchPlayer();
+            }
+            ;
+        }
+    }
+
+    public void createNewGame() {
+        shuffleDeck();
+        this.previousCard = dealCard();
+        System.out.println("Previous Card is " + this.previousCard);
+    }
+
+    protected boolean isSymbolMatch(Card previousCard, Card currentCard) {
+        return previousCard.getSymbol().equals(currentCard.getSymbol());
+    }
+
+    protected void switchPlayer() {
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.playerList.size();
+    }
+
+    protected Player currentPlayer() {
+        return this.playerList.get(currentPlayerIndex);
+    }
+
+    protected void handleSnapInput() {
+        String currentPlayer = currentPlayer().getName();
+        System.out.println(currentPlayer + " snap opportunity, you have 2 sec to type snap to win the game");
+
+        Thread snapThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("\n2 second passed");
+                } catch (InterruptedException e) {
+                    return;
+                }
+
+                System.out.println(currentPlayer + " Time's up! You loss.");
+                System.exit(0);
+            }
+        });
+
+        snapThread.start();
+
+        if (scanner.nextLine().equalsIgnoreCase("snap")) {
+            System.out.println(currentPlayer + ", you win");
+        } else {
+            System.out.println(currentPlayer + ", wrong word, you loss");
+        }
+        snapThread.interrupt();
+    }
+
+    protected Card getPreviousCard() {
         return previousCard;
     }
 
-    public Card getCurrentCard() {
+    protected Card getCurrentCard() {
         return currentCard;
     }
 
@@ -53,70 +119,4 @@ public class Snap extends CardGame {
         return playerList;
     }
 
-    public void createNewGame() {
-        shuffleDeck();
-        this.previousCard = dealCard();
-        System.out.println("Previous Card is " + this.previousCard);
-    }
-
-    public boolean isSymbolMatch(Card previousCard, Card currentCard) {
-        return previousCard.getSymbol().equals(currentCard.getSymbol());
-    }
-
-    protected void switchPlayer() {
-        this.currentPlayerIndex = (this.currentPlayerIndex + 1 ) % this.playerList.size();
-    }
-
-    protected Player currentPlayer(){
-        return this.playerList.get(currentPlayerIndex);
-    }
-
-    protected void handleSnapInput() {
-        String currentPlayer = currentPlayer().getName();
-        System.out.println(currentPlayer + " snap opportunity, you have 2 sec to type snap to win the game");
-
-        Thread snapThread = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                    System.out.println("\n2 second passed");
-                } catch (InterruptedException e) {
-                    return;
-                }
-
-                System.out.println(currentPlayer +" Time's up! You loss.");
-                System.exit(0);
-            }
-        });
-
-        snapThread.start();
-
-            if(scanner.nextLine().equalsIgnoreCase("snap")){
-               System.out.println(currentPlayer + ", you win");
-            }  else {
-                System.out.println(currentPlayer + ", wrong word, you loss");
-            }
-                snapThread.interrupt();
-        }
-
-    public void playGameLoop() {
-        while(true){
-            promptEnterKey(currentPlayer());
-            this.currentCard = dealCard();
-            System.out.println("New Card is " + this.currentCard);
-            if(isSymbolMatch(this.getPreviousCard(), this.getCurrentCard())){
-                handleSnapInput();
-                break;
-            } else if(this.getDeck().isEmpty()){
-                System.out.println("No card left, draw");
-                break;
-            } else {
-                this.previousCard = this.currentCard;
-                System.out.println(currentPlayer().getName() + ", didn't win");
-                System.out.println("Card left in Deck " + this.getDeck().size());
-                switchPlayer();
-            };
-        }
-    }
 }
