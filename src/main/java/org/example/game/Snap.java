ce948ab40e83c7fc5afe6b26a4c4a7b9c535ef0d
edpau <edpau@me.com>
2025-05-04 19:44:33 +0100

    style: improve in-game messages for clarity

--- src/main/java/org/example/game/Snap.java
+++ src/main/java/org/example/game/Snap.java
@@ -30,7 +30,7 @@ public class Snap extends CardGame {
 
     public static void promptEnterKey(Player currentPlayer) {
         while (true) {
-            System.out.println(currentPlayer.getName() + " please press \"ENTER\" to continue...");
+            System.out.println(currentPlayer.getName() + ", your turn! Please press ENTER to draw");
             String input = scanner.nextLine();
             if (input.isEmpty()) {
                 break;
@@ -42,7 +42,7 @@ public class Snap extends CardGame {
         while (true) {
             promptEnterKey(currentPlayer());
             this.currentCard = dealCard();
-            System.out.println("New Card is " + this.currentCard);
+            System.out.println(currentPlayer().getName() + " draws:" + this.currentCard);
             if (isSymbolMatch(this.getPreviousCard(), this.getCurrentCard())) {
                 handleSnapInput();
                 break;
@@ -51,8 +51,8 @@ public class Snap extends CardGame {
                 break;
             } else {
                 this.previousCard = this.currentCard;
-                System.out.println(currentPlayer().getName() + ", didn't win");
-                System.out.println("Card left in Deck " + this.getDeck().size());
+                System.out.println("No match. Next player...");
+                System.out.println("Cards remaining in the deck: " + this.getDeck().size());
                 switchPlayer();
             }
             ;
@@ -62,7 +62,13 @@ public class Snap extends CardGame {
     public void createNewGame() {
         shuffleDeck();
         this.previousCard = dealCard();
-        System.out.println("Previous Card is " + this.previousCard);
+        System.out.println("Welcome to Snap!");
+        System.out.println("Two players will take turns drawing cards");
+        System.out.println("If the value of the new card matches the previous one, SNAP to win!");
+        System.out.println("------------------------------------------");
+        System.out.println("Starting card on the table: " + this.previousCard);
+        System.out.println(currentPlayer().getName() + " will go first");
+        System.out.println();
     }
 
     protected boolean isSymbolMatch(Card previousCard, Card currentCard) {
@@ -79,29 +85,26 @@ public class Snap extends CardGame {
 
     protected void handleSnapInput() {
         String currentPlayer = currentPlayer().getName();
-        System.out.println(currentPlayer + " snap opportunity, you have 2 sec to type snap to win the game");
+        System.out.println(currentPlayer + " : Match found! Type 'snap' within 2 seconds to win!");
 
-        Thread snapThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                    System.out.println("\n2 second passed");
-                } catch (InterruptedException e) {
-                    return;
-                }
-
-                System.out.println(currentPlayer + " Time's up! You loss.");
-                System.exit(0);
+        Thread snapThread = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("\nToo late! 2 seconds are up");
+            } catch (InterruptedException e) {
+                return;
             }
+
+            System.out.println(currentPlayer + ", you missed your chance. You lose. Game End");
+            System.exit(0);
         });
 
         snapThread.start();
 
         if (scanner.nextLine().equalsIgnoreCase("snap")) {
-            System.out.println(currentPlayer + ", you win");
+            System.out.println(currentPlayer + ", you win. Game End");
         } else {
-            System.out.println(currentPlayer + ", wrong word, you loss");
+            System.out.println(currentPlayer + ", incorrect input. You lose the game. Game End");
         }
         snapThread.interrupt();
     }
